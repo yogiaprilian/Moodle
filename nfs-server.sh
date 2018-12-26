9191e079193984c0cc672f4d21a41cbb2967c718
yogiaprilian <45745493+yogiaprilian@users.noreply.github.com>
2018-12-26 08:55:08 +0700

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt-get -y install nfs-kernel-server
 
 #create folder
-sudo mkdir /home/erudeye/nfs/website -p
+sudo mkdir /home/erudeye/nfs -p
 
 #give access permission
-sudo chown nobody:nogroup /home/erudeye/nfs/website
-sudo chmod 777 /home/erudeye/nfs/website
+sudo chown nobody:nogroup /home/erudeye/nfs
+sudo chmod 777 /home/erudeye/nfs
 
 #mv /etc/exports
 sudo mv /etc/exports /home/erudeye
@@ -17,3 +17,4 @@ sudo mv /etc/exports /home/erudeye
 sudo wget https://github.com/yogiaprilian/NFS-Ubuntu/blob/master/exportsnfs -P /etc/exports
 
 sudo systemctl restart nfs-kernel-server
+
