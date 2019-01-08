0671164143f55f4e97fdb8d2285ac0c62e92388a
yogiaprilian <45745493+yogiaprilian@users.noreply.github.com>
2019-01-08 09:56:13 +0700

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt-get -y install nfs-kernel-server
 
 #create folder
-sudo mkdir /home/erudeye/nfs -p
+sudo mkdir /home/erudeye/nfs/website -p
 
 #give access permission
-sudo chown nobody:nogroup /home/erudeye/nfs
-sudo chmod 777 /home/erudeye/nfs
+sudo chown nobody:nogroup /home/erudeye/nfs/website
+sudo chmod 777 /home/erudeye/nfs/website
 
 #mv /etc/exports
 sudo mv /etc/exports /var/backups/
